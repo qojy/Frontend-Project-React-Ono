69964ca553cf5566b6dd0a132495e39499c9bdf0
qojy <noamlivne5@gmail.com>
2025-03-11 21:58:35 +0200

    test1.1

---
+++ code/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <AppBar>
+      <ToolBar>
+        <Typography variant="h6" component="div">
+          Header
+        </Typography>
+      </ToolBar>
+    </AppBar>
+  );
+}
