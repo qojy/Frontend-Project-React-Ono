ec2c59221b7fe325af41b28eaf0deb471d62d7f4
Tal <tal.saar@365.ono.ac.il>
2025-03-18 22:13:54 +0200

    Added components according to task

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -2,6 +2,13 @@ import Header from "./Components/Header.jsx";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Components/Homepage.jsx";
 import Help from "./Components/Help.jsx";
+import ActiveOrders from "./Components/ActiveOrders.jsx";
+import ManageClasses from "./Components/ManageClasses.jsx";
+import Menu from "./Components/Menu.jsx";
+import NewOrder from "./Components/NewOrder.jsx";
+import OrderHistory from "./Components/OrderHistory.jsx";
+import OrderStatus from "./Components/OrderStatus.jsx";
+import StudentsList from "./Components/StudentsList.jsx";
 function App() {
   return (
     <>
@@ -9,6 +16,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/help" element={<Help />} />
+        <Route path="/ActiveOrders" element={<ActiveOrders />} />
+        <Route path="/ManageClasses" element={<ManageClasses />} />
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/NewOrder" element={<NewOrder />} />
+        <Route path="/OrderHistory" element={<OrderHistory />} />
+        <Route path="/OrderStatus" element={<OrderStatus />} />
+        <Route path="/StudentList" element={<StudentsList />} />
       </Routes>
     </>
   );
