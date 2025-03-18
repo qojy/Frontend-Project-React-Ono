d35cd2a93f71f175246592fbb47f5ec446d29228
qojy <noamlivne5@gmail.com>
2025-03-18 21:46:19 +0200

    Add header, Help and Homepage Components

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,8 +1,15 @@
-import Header from "./Header.jsx";
+import Header from "./Components/Header.jsx";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Components/Homepage.jsx";
+import Help from "./Components/Help.jsx";
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/help" element={<Help />} />
+      </Routes>
     </>
   );
 }
--- code/src/Components/Header.jsx
+++ code/src/Components/Header.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export default function Header() {
   return (
     <AppBar>
-      <ToolBar>
+      <Toolbar>
         <Typography variant="h6" component="div">
           Header
         </Typography>
-      </ToolBar>
+      </Toolbar>
     </AppBar>
   );
 }
